c08abdf027c4951d2a0f26afd041975a40c66fff
Mitiku Yohannes <se.mitiku.yohannes@gmail.com>
2024-12-25 17:20:58 +0000

    Update setup.py and README

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@ setup(
     name="file-structure-py",
     version="1.0.0",
     description="A Python utility for creating and generating file structures.",
-    author="Your Name",
-    author_email="your.email@example.com",
-    packages=find_packages(),
+    author="Mitiku Yohannes",
+    author_email="se.mitiku.yohannes@gmail.com",
+    url="https://github.com/ymitiku/file-structure-py",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.8",
     entry_points={
         "console_scripts": [
             "file-structure-py=file_structure_py.main:main",
