fd4e0124a9afbc9ec8d9f0683ca4e787e088caf1
Mitiku Yohannes <se.mitiku.yohannes@gmail.com>
2024-12-25 17:27:05 +0000

    Update the version to 1.0.2

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="file-structure-py",
-    version="1.0.0",
+    version="1.0.2",
     description="A Python utility for creating and generating file structures.",
     author="Mitiku Yohannes",
     author_email="se.mitiku.yohannes@gmail.com",
     url="https://github.com/ymitiku/file-structure-py",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(where="./"),
+    package_dir={"": "."},
     python_requires=">=3.8",
     entry_points={
         "console_scripts": [
